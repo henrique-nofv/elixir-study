68c771c934383fa4d8f1ecec37f8ff3a2d613618
Henrique <henrique.nofv@gmail.com>
2024-06-21 12:35:06 -0300

    private functions

--- fizz_buzz/lib/fizz_buzz.ex
+++ fizz_buzz/lib/fizz_buzz.ex
@@ -19,23 +19,23 @@ defmodule FizzBuzz do
     |> handle_file
   end
 
-  def handle_file{:ok, result} do
+  defp handle_file{:ok, result} do
     result
     |> String.split(",")
     |> Enum.map(&convert_and_evaluate/1)
   end
 
-  def handle_file{:error, reason} do "Failed, reason: #{reason}" end
+  defp handle_file{:error, reason} do "Failed, reason: #{reason}" end
 
-  def convert_and_evaluate(elem) do
+  defp convert_and_evaluate(elem) do
     elem
     |> String.trim()
     |> String.to_integer()
     |> evaluate_number
   end
 
-  def evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
-  def evaluate_number(number) when rem(number, 3) == 0, do: :fizz
-  def evaluate_number(number) when rem(number, 5) == 0, do: :buzz
-  def evaluate_number(number), do: number
+  defp evaluate_number(number) when rem(number, 3) == 0 and rem(number, 5) == 0, do: :fizzbuzz
+  defp evaluate_number(number) when rem(number, 3) == 0, do: :fizz
+  defp evaluate_number(number) when rem(number, 5) == 0, do: :buzz
+  defp evaluate_number(number), do: number
 end
