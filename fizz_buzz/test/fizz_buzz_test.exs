91477763553c3f14789489322f510b3e5795464a
Henrique <henrique.nofv@gmail.com>
2024-06-21 12:43:33 -0300

    add unit tests

--- fizz_buzz/test/fizz_buzz_test.exs
+++ fizz_buzz/test/fizz_buzz_test.exs
@@ -1,8 +1,17 @@
 defmodule FizzBuzzTest do
   use ExUnit.Case
-  doctest FizzBuzz
 
-  test "greets the world" do
-    assert FizzBuzz.hello() == :world
+  describe "format/1" do
+    test "when a valid file is provided, returns the converted list" do
+      expected_response = {:ok, [1, 2, :fizz, 4, :buzz, :fizzbuzz]}
+
+      assert FizzBuzz.format("file.txt") == expected_response
+    end
+
+    test "when a ivalid file is provided, returns an error" do
+      expected_response = {:error, "Failed, reason: enoent"}
+
+      assert FizzBuzz.format("invalid.txt") == expected_response
+    end
   end
 end
